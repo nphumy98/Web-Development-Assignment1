c8a9170d656f4808c05bb55d40b01a9348c77d51
Jimmy Nguyen <nphumy89@gmail.com>
2019-04-01 06:00:10 +1300

    check table exist

--- poststatusprocess.php
+++ poststatusprocess.php
@@ -50,15 +50,13 @@
         //check status code is unique
         function is_status_code_unique($statusCode, $sql_tble, $conn)
         {
-          $query = "select * from $sql_tble where status_code = '$statusCode'";
+          $query_check_status_code_unqie = "select * from $sql_tble where status_code = '$statusCode'";
           // executes the query
-          echo $query;
-      		$result = mysqli_query($conn, $query);
+      		$result = mysqli_query($conn, $query_check_status_code_unqie);
           $rowCount= mysqli_num_rows($result);
           //if the status code already in the table
           if ( $rowCount== 0)
           {
-            echo "<p>The Status Code not in the table. the row count is ", $rowCount,"</p>";
               return true;
           }
           else
@@ -87,57 +85,71 @@
         $allowComment= check_boolean_value($_POST['allowComment']);
         $allowShare= check_boolean_value($_POST['allowShare']);
 
+        // check status code and status format
+        $statusCodeFormat=check_status_code_format($statusCode);
+        $statusFormat= check_status_format($status);
 
-        check_status_code_format($statusCode);
-        check_status_format($status);
-
-        $sql_host="cmslamp14.aut.ac.nz";
-        $sql_user="wrk2544";
-        $sql_pass="6889hong";
-        $sql_db="wrk2544";
-        $sql_tble="status_post";
-        // connect database
-        // The @ operator suppresses the display of any error messages
-        // mysqli_connect returns false if connection failed, otherwise a connection value
-        $conn = @mysqli_connect($sql_host,
-          $sql_user,
-          $sql_pass,
-          $sql_db
-        );
-
-        // Checks if connection is successful
-        if (!$conn)
+        if(($statusCodeFormat)&&($statusFormat))
         {
-          // Displays an error message
-          echo "<p>Database connection failure</p>";
-        }
-        else
-        {
-          echo "<p>Database connection sucess</p>";
-          //check the status code if it is unique
-          is_status_code_unique($statusCode, $sql_tble, $conn);
-      		// Set up the SQL command to add the data into the table
-      		$query = "insert into $sql_tble"
-      						."(status_code, status, share, post_date, allow_like, allow_comment, allow_share)"
-      					. "values"
-      						."('$statusCode','$status','$share', '$post_date','$allowLike','$allowComment','$allowShare')";
-        echo $query;
-      		// executes the query
-      		$result = mysqli_query($conn, $query);
-      		// checks if the execution was successful
-      		if(!$result) {
-      			echo "<p>Something is wrong with ",	$query, "</p>";
-      		} else {
-      			// display an operation successful message
-      			echo "<p>Success add</p>";
-      		} // if successful query operation
+          //database infor
+          $sql_host="cmslamp14.aut.ac.nz";
+          $sql_user="wrk2544";
+          $sql_pass="6889hong";
+          $sql_db="wrk2544";
+          $sql_tble="status_post";
+          // connect database
+          // The @ operator suppresses the display of any error messages
+          // mysqli_connect returns false if connection failed, otherwise a connection value
+          $conn = @mysqli_connect($sql_host,
+            $sql_user,
+            $sql_pass,
+            $sql_db
+          );
+
+          // Checks if connection is successful
+          if (!$conn)
+          {
+            // Displays an error message
+            echo "<p>Database connection failure</p>";
+          }
+          else
+          {
+            echo "<p>Database connection sucess</p>";
+            //check if the table is exsit
+            $query_check_table = "DESCRIBE $sql_tble";
+            //if the table exist
+            if(mysqli_query($conn,$query_check_table)!=false)
+            {
+              //check the status code if it is unique
+              //if status code is unique
+              $is_status_code_unique=is_status_code_unique($statusCode, $sql_tble, $conn);
+              if($is_status_code_unique)
+              {
+                // Set up the SQL command to add the data into the table
+               $query_insert = "insert into $sql_tble"
+                       ."(status_code, status, share, post_date, allow_like, allow_comment, allow_share)"
+                     . "values"
+                       ."('$statusCode','$status','$share', '$post_date','$allowLike','$allowComment','$allowShare')";
+               // executes the query
+               $result = mysqli_query($conn, $query_insert);
+               // checks if the execution was successful
+               if(!$result) {
+                 echo "<p>Something is wrong with ",	$query_insert, "</p>";
+               } else {
+                 // display an operation successful message
+                 echo "<p>Success add</p>";
+               } // if successful query operation
+              }
+            }
+            else
+            {
+              echo "<p>The table: ",	$sql_tble, " is not existed</p>";
+            }
+        		// close the database connection
+        		mysqli_close($conn);
+          }
           //link to homepage and poststatusform
           echo "<p><a href="."poststatusform.php".">Add another Status</a>"."</p>";
           echo "<p><a href="."index.html".">Return to Home Page</a>"."</p>";
-      		// close the database connection
-      		mysqli_close($conn);
-
-
-
         }
 ?>
--- searchstatusprocess.php
+++ searchstatusprocess.php
@@ -30,58 +30,67 @@
   else
   {
 		// Upon successful connection
+    //check if table exist
+    $query_check_table = "DESCRIBE $sql_tble";
+    //if the table exist
+    if(mysqli_query($conn,$query_check_table)!=false)
+    {
+      //check if the status string is null or empty
+      check_input_empty();
+      // Get data from the form
+      $status= $_GET['status'];
 
-    //check if the status string is null or empty
-		check_input_empty();
-		// Get data from the form
-		$status= $_GET['status'];
+      // Set up the SQL command to retrieve the data from the table
+      // % symbol represent a wildcard to match any characters
+      // like is a compairson operator
+      $query = "select * from $sql_tble where status like '%$status%'";
 
-		// Set up the SQL command to retrieve the data from the table
-		// % symbol represent a wildcard to match any characters
-		// like is a compairson operator
-		$query = "select * from $sql_tble where status like '%$status%'";
-
-		// executes the query and store result into the result pointer
-		$result = mysqli_query($conn, $query);
-		// checks if the execuion was successful
-		if(!$result) {
-			echo "<p>Something is wrong with ",	$query, "</p>";
-		} else {
-			// Display the retrieved records
-			echo "<table border=\"1\">";
-			echo "<tr>\n"
-				 ."<th scope=\"col\">Status Code</th>\n"
-			   ."<th scope=\"col\">Status</th>\n"
-				 ."<th scope=\"col\">Share</th>\n"
-				 ."<th scope=\"col\">Post Date</th>\n"
-         ."<th scope=\"col\">Allow Like</th>\n"
-         ."<th scope=\"col\">Allow Comment</th>\n"
-         ."<th scope=\"col\">Allow Share</th>\n"
-				 ."</tr>\n";
-			// retrieve current record pointed by the result pointer
-			// Note the = is used to assign the record value to variable $row, this is not an error
-			// the ($row = mysqli_fetch_assoc($result)) operation results to false if no record was retrieved
-			// _assoc is used instead of _row, so field name can be used
-			while ($row = mysqli_fetch_assoc($result)){
-        //fix format for post date
-        $post_date= $row["post_date"];
-        $date = str_replace('-', '/', $post_date);
-        $date= date('d/m/Y', strtotime($date));
-        //print table row
-				echo "<tr>";
-				echo "<td>",$row["status_code"],"</td>";
-				echo "<td>",$row["status"],"</td>";
-				echo "<td>",$row["share"],"</td>";
-				echo "<td>",$date,"</td>";
-        echo "<td>",$row["allow_like"],"</td>";
-        echo "<td>",$row["allow_comment"],"</td>";
-        echo "<td>",$row["allow_share"],"</td>";
-				echo "</tr>";
-			}
-			echo "</table>";
-			// Frees up the memory, after using the result pointer
-			mysqli_free_result($result);
-		} // if successful query operation
+      // executes the query and store result into the result pointer
+      $result = mysqli_query($conn, $query);
+      // checks if the execuion was successful
+      if(!$result) {
+        echo "<p>Something is wrong with ",	$query, "</p>";
+      } else {
+        // Display the retrieved records
+        echo "<table border=\"1\">";
+        echo "<tr>\n"
+           ."<th scope=\"col\">Status Code</th>\n"
+           ."<th scope=\"col\">Status</th>\n"
+           ."<th scope=\"col\">Share</th>\n"
+           ."<th scope=\"col\">Post Date</th>\n"
+           ."<th scope=\"col\">Allow Like</th>\n"
+           ."<th scope=\"col\">Allow Comment</th>\n"
+           ."<th scope=\"col\">Allow Share</th>\n"
+           ."</tr>\n";
+        // retrieve current record pointed by the result pointer
+        // Note the = is used to assign the record value to variable $row, this is not an error
+        // the ($row = mysqli_fetch_assoc($result)) operation results to false if no record was retrieved
+        // _assoc is used instead of _row, so field name can be used
+        while ($row = mysqli_fetch_assoc($result)){
+          //fix format for post date
+          $post_date= $row["post_date"];
+          $date = str_replace('-', '/', $post_date);
+          $date= date('d/m/Y', strtotime($date));
+          //print table row
+          echo "<tr>";
+          echo "<td>",$row["status_code"],"</td>";
+          echo "<td>",$row["status"],"</td>";
+          echo "<td>",$row["share"],"</td>";
+          echo "<td>",$date,"</td>";
+          echo "<td>",$row["allow_like"],"</td>";
+          echo "<td>",$row["allow_comment"],"</td>";
+          echo "<td>",$row["allow_share"],"</td>";
+          echo "</tr>";
+        }
+        echo "</table>";
+        // Frees up the memory, after using the result pointer
+        mysqli_free_result($result);
+      } // if successful query operation
+    }
+    else
+    {
+      echo "<p>The table: ",	$sql_tble, " is not existed</p>";
+    }
     // close the database connection
 		mysqli_close($conn);
 	} // if successful database connection
